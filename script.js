4b29cd1b7e6677feab5b67f9b0a831d13f214106
Mafalda Rosa <mafalda.rosa.sa@gmail.com>
2026-07-22 19:07:18 +0100

    Created "Contact me"

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// Ações para o About Me
 const btnAbout = document.getElementById("btnAbout");
 const aboutSection = document.getElementById("aboutSection");
 
 btnAbout.addEventListener("click", () => {
   aboutSection.classList.toggle("hidden");
+});
+
+// Ações para o Contact Me
+const btnContact = document.getElementById("btnContact");
+const contactSection = document.getElementById("contactSection");
+
+btnContact.addEventListener("click", () => {
+  contactSection.classList.toggle("hidden");
 });
